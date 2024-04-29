94b4adb72226586c5a631733f7c8455c339e97e9
Bishnu Bhattarai <bishnusyangja@gmail.com>
2024-04-29 10:17:50 +0545

    login fix

--- front_app/src/components/loginForm.js
+++ front_app/src/components/loginForm.js
@@ -1,76 +1,87 @@
 import React from 'react';
 import { Button, Checkbox, Form, Input } from 'antd';
+import Request from '../api';
+import {Redirect} from 'react-router-dom';
 
-const onFinish = (values) => {
-  console.log('Success:', values);
+
+const LoginForm = () => {
+    const onFinish = (values) => {
+        const url = '/api-auth-token/';
+        Request.post(url, values, {
+        }).then(function (response) {
+            localStorage.setItem('authToken', response.data.token);
+            window.location.href = '/book-list';
+        }).catch(function (error) {
+            console.log(error);
+        })
+    };
+
+    const onFinishFailed = (errorInfo) => {
+      console.log('Failed:', errorInfo);
+    };
+    return (
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item >
+          <span>
+          Don't Have account <a href="/register" >Register </a>
+          </span>
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    )
 };
-
-const onFinishFailed = (errorInfo) => {
-  console.log('Failed:', errorInfo);
-};
-
-const LoginForm = () => (
-  <Form
-    name="basic"
-    labelCol={{
-      span: 8,
-    }}
-    wrapperCol={{
-      span: 16,
-    }}
-    style={{
-      maxWidth: 600,
-    }}
-    initialValues={{
-      remember: true,
-    }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item
-      label="Username"
-      name="username"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your username!',
-        },
-      ]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item
-      label="Password"
-      name="password"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your password!',
-        },
-      ]}
-    >
-      <Input.Password />
-    </Form.Item>
-
-    <Form.Item >
-      <span>
-      Don't Have account <a href="/register" >Register </a>
-      </span>
-    </Form.Item>
-
-    <Form.Item
-      wrapperCol={{
-        offset: 8,
-        span: 16,
-      }}
-    >
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-);
 
 export default LoginForm;
--- libapp/libapp/urls.py
+++ libapp/libapp/urls.py
@@ -22,7 +22,7 @@ from user.views import RegisterUserAPI
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-token-auth/', obtain_jwt_token, name='jwt_token'),
+    path('api-auth-token/', obtain_jwt_token, name='jwt_token'),
     path('register/', RegisterUserAPI.as_view(), name='register'),
 
 ]
