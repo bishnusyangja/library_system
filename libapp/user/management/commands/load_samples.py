aad4de1683d649f6a7d769ffd83c5c69036092c7
Bishnu Bhattarai <bishnusyangja@gmail.com>
2024-04-29 11:50:28 +0545

    sample data loading

---
+++ libapp/user/management/commands/load_samples.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from user.models import User
+from book.models import Book
+from model_bakery import baker
+
+
+class Command(BaseCommand):
+    help = 'Loads sample data'
+
+    def handle(self, *args, **options):
+        abc_user = baker.make(User, first_name='ABC', last_name='Sharma', email='abc@example.com')
+        self.stdout.write('Created user successfylly ... ')
+
+        baker.make(Book, name='Game Play')
+        baker.make(Book, name='Writing and Speaking')
+        baker.make(Book, name='Reading and Speaking')
+
+        self.stdout.write('Created books sucessfully ... ')
